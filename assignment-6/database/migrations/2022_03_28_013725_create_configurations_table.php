eb59fc91f20147fcfd3a483af6339e864fe5770a
Nick Raskop <raskop@usc.edu>
2022-03-27 19:53:57 -0700

    migrate

--- assignment-6/database/migrations/2022_03_28_013725_create_configurations_table.php
+++ assignment-6/database/migrations/2022_03_28_013725_create_configurations_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
+        Schema::dropIfExists('configurations');
         Schema::create('configurations', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
