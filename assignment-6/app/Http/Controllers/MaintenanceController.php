71299946969e8336f2bb7ac2b966508bbf151eba
Nick Raskop <raskop@usc.edu>
2022-03-29 02:20:27 -0700

    Maintenance controller

--- assignment-6/app/Http/Controllers/MaintenanceController.php
+++ assignment-6/app/Http/Controllers/MaintenanceController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Configuration;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -10,5 +11,34 @@ class MaintenanceController extends Controller
     public function index()
     {
       return view('maintenance.index');
+    }
+
+    public function secret()
+    {
+      $maintenance = Configuration::where('name', '=', 'maintenance-mode')->first();
+
+      return view('maintenance.edit', [
+        'maintenance' => $maintenance,
+        'val' => $maintenance->value,
+      ]);
+    }
+
+    public function update(Request $request)
+    {
+      $val = false;
+      if ($request->input('value') == 'on') {
+        $val = true;
+      }
+      $config = Configuration::where('name', '=', 'maintenance-mode')->first();
+      $config->value = $val;
+      $config->save();
+
+      return redirect()
+        ->route('maintenance.edit');
+    }
+
+    public function test()
+    {
+      return view('maintenance.test');
     }
 }
