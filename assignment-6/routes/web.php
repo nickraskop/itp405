9fe85a2399fc7dfb70e1805ef25dea228de6b502
Nick Raskop <raskop@usc.edu>
2022-04-09 15:53:31 -0700

    URL

--- assignment-6/routes/web.php
+++ assignment-6/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MaintenanceController;
 use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\URL;
 
 /*
 |--------------------------------------------------------------------------
