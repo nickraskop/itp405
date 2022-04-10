f922cc4455bdb1685f1a0fc7b5b35e8b4e5b08d6
Nick Raskop <raskop@usc.edu>
2022-04-09 22:22:08 -0700

    Authentication and middleware

--- assignment-7/routes/web.php
+++ assignment-7/routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AlbumController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\URL;
 
 /*
@@ -19,11 +22,23 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/albums', [AlbumController::class, 'index'])->name('album.index');
+Route::middleware(['auth'])->group(function() {
+    Route::get('/profile', [ProfileController::class, 'index'])->name('profile.index');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout');
+    Route::get('/albums', [AlbumController::class, 'index'])->name('album.index');
+});
+
 Route::get('/albums/new', [AlbumController::class, 'create'])->name('album.create');
 Route::post('/albums', [AlbumController::class, 'store'])->name('album.store');
 Route::get('/albums/{id}/edit', [AlbumController::class, 'edit'])->name('album.edit');
 Route::post('/albums/{id}', [AlbumController::class, 'update'])->name('album.update');
+
+Route::get('/register', [RegisterController::class, 'index'])->name('registration.index');
+Route::post('/register', [RegisterController::class, 'register'])->name('registration.create');
+
+Route::get('/login', [AuthController::class, 'loginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
+
 
 if (env('APP_ENV') !== 'local') {
     URL::forceScheme('https');
