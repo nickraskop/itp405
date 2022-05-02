3700a03cd85af78a47f8e75f731278590a1ba7e1
Nick Raskop <raskop@usc.edu>
2022-05-02 01:21:00 -0700

    home page ip

--- final-project-raskop/app/Http/Controllers/HomeController.php
+++ final-project-raskop/app/Http/Controllers/HomeController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\Post;
 
 class HomeController extends Controller
 {
     public function index()
     {
-      return view('home.index');
+      $posts = Post::all();
+      return view('home.index', [
+        'posts' => $posts,
+      ]);
     }
 }
--- final-project-raskop/resources/views/home/index.blade.php
+++ final-project-raskop/resources/views/home/index.blade.php
@@ -1,5 +1,43 @@
 @extends("layouts.main")
 
 @section("content")
-  <h1>Home</h1>
+  <style>
+    #post {
+      border-color: #d5d8de;
+      border-style: solid;
+      border-width: 1px;
+      margin-top: 30px;
+      margin-bottom: 30px;
+      padding: 10px;
+      display: flex;
+      flex-direction: column;
+      width: 50%;
+      margin-left: auto;
+      margin-right: auto;
+    }
+    #post img {
+      width: 100%;
+      height: 100%;
+    }
+    #user {
+      display: flex;
+      align-items: center;
+    }
+    #user img {
+      border-radius: 100%;
+      width: 35px;
+      height: 35px;
+      margin-right: 10px;
+    }
+  </style>
+  @foreach($posts as $post)
+    <div id="post">
+      <div id="user">
+        <img src="{{ $post->user->pfp }}" alt="">
+        <h3>{{ $post->user->name }}</h3>
+      </div>
+      <img src="{{ $post->photo }}" alt="">
+      <p>{{ $post->caption }}</p>
+    </div>
+  @endforeach
 @endsection
